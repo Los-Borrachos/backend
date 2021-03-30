ef00356b177a5922da893979a4904c6adbf946c7
NickAponte <apontenick@aol.com>
2021-03-30 13:59:23 -0400

    added app.use to index, tested get and out routes for clients

--- controllers/Client.js
+++ controllers/Client.js
@@ -3,25 +3,25 @@ const Client = require('../models/Client');
 
 const router = express.Router();
 // Grabs and sends all records
-router.get('/clients/', (req, res) => {
+router.get('/', (req, res) => {
 	Client.find({}).then((record) => {
 		res.json(record);
 	});
 });
 // Grab by id
-router.get('/clients/:id', (req, res) => {
+router.get('/:id', (req, res) => {
 	Client.findById({ _id: req.params.id }).then((record) => {
 		res.json(record);
 	});
 });
 // post new client record
-router.post('/clients', (req, res) => {
+router.post('/', (req, res) => {
 	Client.create(req.body).then((record) => {
 		res.json(record);
 	});
 });
 // delete by id
-router.delete('/clients/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 	Client.findByIdAndDelete({ _id: req.params.id }).then((delRecord) => {
 		res.json(delRecord);
 	});
