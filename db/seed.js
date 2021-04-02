ed61db2b7108d072f7b4b7ac2f9de2a0ffdf944f
ethanfincher <ethan.j.fincher@gmail.com>
2021-04-02 06:59:03 -0500

    added seed data for prospects

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,23 @@
-const clients = require('./clients.json')
-
+const clients = require('./clients.json');
 const Client = require('../models/Client');
 
+const prospects = require('./prospects.json');
+const Prospect = require('../models/Prospect');
+
 Client.deleteMany({})
-    .then(() => {
-        return Client.insertMany(clients); 
-    })
-    .catch(console.error)
-    .finally(() => {
-        process.exit(); 
-    })
+	.then(() => {
+		return Client.insertMany(clients);
+	})
+	.catch(console.error)
+	.finally(() => {
+		process.exit();
+	});
+
+Prospect.deleteMany({})
+	.then(() => {
+		return Prospect.insertMany(prospects);
+	})
+	.catch(console.error)
+	.finally(() => {
+		process.exit();
+	});
