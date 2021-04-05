da10c31585209f6ea6ebb21681d572b5e23453b0
NickAponte <apontenick@aol.com>
2021-04-05 11:08:55 -0400

    update connection string

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose')
-
-mongoose.Promise = Promise
-const dbuser = 'primary-user';
-const password = 'gmpgRLHOOEOakJLF';
-const dbName = 'hound'
-const mongoURI = `mongodb+srv://${dbuser}:${password}@cluster0.xr8bv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-
-mongoose.connect(mongoURI, { useNewUrlParser: true })
-.then((conn) => {
-	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
-})
-.catch(err => {
-	console.error(err)
-})
-
-module.exports = mongoose
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+const mongoURI = process.env.DB_URL;
+mongoose
+	.connect(mongoURI, { useNewUrlParser: true })
+	.then((conn) => {
+		console.log(`connected to mongodb on ${conn.connections[0].name} db`);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+module.exports = mongoose;
